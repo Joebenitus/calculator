c539bc400dd0a18e393111319dc1bb6b97f1a00e
Joseph Karnafel <josephkarnafel@gmail.com>
2020-08-12 13:54:03 -0700

    add options for subtraction, multiplication and division
    Co-authored-by: Adam Mansell <adammansellhmb@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,50 @@
-function add(num1, num2) {
-	return num1 + num2;
+// Business logic:
+
+function add(number1, number2) {
+  return number1 + number2;
 }
 
-const num1 = parseInt(prompt("Enter a number: "));
-const num2 = parseInt(prompt("Enter another number: "));
+function subtract(number1, number2) {
+  return number1 - number2;
+}
 
-alert(add(num1, num2));
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+// User interface logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+   event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+     const number1 = parseInt($("#sub1").val());
+     const number2 = parseInt($("#sub2").val());
+     const result = subtract(number1, number2);
+     $("#output").text(result);
+   });
+   $("form#multiply").submit(function(event) {
+    event.preventDefault();
+     const number1 = parseInt($("#mul1").val());
+     const number2 = parseInt($("#mul2").val());
+     const result = multiply(number1, number2);
+     $("#output").text(result);
+   });
+   $("form#divide").submit(function(event) {
+    event.preventDefault();
+     const number1 = parseInt($("#div1").val());
+     const number2 = parseInt($("#div2").val());
+     const result = divide(number1, number2);
+     $("#output").text(result);
+   });
+});
